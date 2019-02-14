0cfe191e3f9ac297c95abec83b9323eadf6356da
Yannis Barlas <yannisbarlas@gmail.com>
2019-02-14 18:10:14 +0200

    feat(dashboard): build sorting ui and add state handlers

--- packages/dashboard/src/SortMenu.js
+++ packages/dashboard/src/SortMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { State } from 'react-powerplug'
 
 import { Menu as UIMenu } from '@pubsweet/ui'
 
@@ -14,6 +15,33 @@ const SortIcon = styled.div`
   width: 24px;
 `
 
+const triangle = css`
+  background: #3f3f3f;
+  content: ' ';
+  display: block;
+  height: 15px;
+  position: absolute;
+  transition: 0.2s ease-in-out;
+  width: 15px;
+  z-index: 200;
+`
+
+const triangleLeft = css`
+  ${triangle};
+  clip-path: polygon(49% 49%, 0 0, 0 100%);
+`
+
+const triangleUp = css`
+  ${triangle};
+  clip-path: polygon(0% 100%, 50% 50%, 100% 100%);
+`
+
+const triangleOption = css`
+  ${triangleLeft};
+  left: 0;
+  top: 8px;
+`
+
 const Menu = styled(UIMenu)`
   display: inline-flex;
 
@@ -24,7 +52,7 @@ const Menu = styled(UIMenu)`
       left: 50%;
       width: 300px;
       transform: translate(-50%, 0);
-      z-index: 1;
+      z-index: 100;
     }
 
     div[open] {
@@ -35,56 +63,139 @@ const Menu = styled(UIMenu)`
       margin-top: 16px;
       position: relative;
       overflow-y: unset;
+      text-transform: uppercase;
       width: 300px;
 
       &::before {
-        height: 15px;
-        width: 15px;
-
-        content: ' ';
-        display: block;
-        background: #3f3f3f;
-        clip-path: polygon(0% 100%, 50% 50%, 100% 100%);
-        position: absolute;
-        top: -19px;
+        ${triangleUp}
         left: calc(50% - 15px / 2);
-        z-index: 2000;
+        top: -19px;
       }
     }
 
     div[role='option'] {
-      padding: 4px 8px;
+      cursor: pointer;
+      font-family: 'Fira Sans Condensed';
+      padding: 4px 4px 4px 12px;
+      position: relative;
+
+      &::before {
+        ${triangleOption}
+        opacity: 0;
+      }
+
+      &[aria-selected='true'] {
+        color: #0d78f2;
+        font-weight: normal;
+
+        &::before {
+          /* ${triangleOption} */
+          background: #0d78f2;
+          /* opa */
+        }
+      }
 
       &::selection {
         background: none;
       }
 
       &:hover {
-        background: #f1f1f1;
+        background: #fafafa;
+        color: #0d78f2;
+        transition: 0.2s ease-in-out;
+
+        &::before {
+          /* ${triangleOption} */
+          background: gray;
+          opacity: 1;
+        }
       }
     }
   }
 `
 
-const IconOpener = ({ toggleMenu }) => (
-  <SortIcon onClick={toggleMenu} title="Click to sort books" />
-)
+const OpenerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  > span {
+    font-family: 'Fira Sans Condensed';
+    margin-right: 8px;
+    text-transform: uppercase;
+
+    span {
+      cursor: pointer;
+      font-weight: 500;
+    }
+  }
+`
+
+const Opener = props => {
+  const { ascending, changeOrder, selected, toggleMenu } = props
+
+  return (
+    <OpenerWrapper>
+      <span>
+        Sort By <span onClick={toggleMenu}>{selected}</span>
+      </span>
+
+      <SortIcon
+        ascending={ascending}
+        onClick={changeOrder}
+        title={ascending ? 'Ascending' : 'Descending'}
+      />
+    </OpenerWrapper>
+  )
+}
 
 const options = [
   {
-    label: 'one',
-    value: 1,
+    label: 'title',
+    value: 'title',
   },
   {
-    label: 'two',
-    value: 2,
+    label: 'author',
+    value: 'author',
   },
-  {
-    label: 'i am long so long this is ridiculous',
-    value: 3,
-  },
+  // {
+  //   label: 'i am long so long this is ridiculous',
+  //   value: 3,
+  // },
 ]
 
-const SortMenu = () => <Menu options={options} renderOpener={IconOpener} />
+const SortMenu = () => {
+  const sendIt = (value, ascending) => {
+    console.log(value, ascending)
+  }
+
+  return (
+    <State initial={{ ascending: true, value: 'title' }}>
+      {({ state, setState }) => {
+        const { ascending, value } = state
+
+        const handleChange = value => {
+          // console.log(value, ascending)
+          setState({ value })
+        }
+
+        const changeOrder = () => {
+          setState({ ascending: !state.ascending })
+          // handleChange()
+        }
+
+        return (
+          <Menu
+            ascending={ascending}
+            changeOrder={changeOrder}
+            onChange={handleChange}
+            options={options}
+            renderOpener={Opener}
+            selected={value}
+          />
+        )
+      }}
+    </State>
+  )
+}
 
 export default SortMenu
