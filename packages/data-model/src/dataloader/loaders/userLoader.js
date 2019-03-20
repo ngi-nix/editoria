7891594d779ea10140f4d8f085d609207a5a33ac
Giannis Kopanas <jkopanas@gmail.com>
2019-03-20 19:03:08 +0200

    fix(authsome): lint problem

--- packages/data-model/src/dataloader/loaders/userLoader.js
+++ packages/data-model/src/dataloader/loaders/userLoader.js
@@ -2,7 +2,13 @@ const Loader = require('../loader')
 const { model } = require('@pubsweet/model-user')
 
 const UserLoader = {
-  userTeams: new Loader(async id => await model.find(id, {eager: 'teams'})),
+  userTeams: new Loader(
+    async id =>
+      // eslint-disable-next-line no-return-await
+      await model.find(id, {
+        eager: 'teams',
+      }),
+  ),
 }
 
 module.exports = UserLoader
