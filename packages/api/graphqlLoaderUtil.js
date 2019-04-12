3e62108c9a98805f965b8ecb2e67c5dc3306839a
Alexandros Georgantas <alexgeorg86@gmail.com>
2019-04-12 19:34:45 +0300

    fix(editoria-api): path fix

--- packages/api/graphqlLoaderUtil.js
+++ packages/api/graphqlLoaderUtil.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 const loadGQLFile = type => {
-  const filePath = path.join(__dirname, '../api', type)
+  const filePath = path.join(__dirname, type)
   return fs.readFileSync(filePath, 'utf-8')
 }
 
