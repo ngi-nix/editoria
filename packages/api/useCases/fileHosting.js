2731a3a7c5ba8948ff2582fe96d47ca8fdece550
Alexandros Georgantas <alexgeorg86@gmail.com>
2020-02-11 15:50:40 +0200

    improvement(api): more features for s3

--- packages/api/useCases/fileHosting.js
+++ packages/api/useCases/fileHosting.js
@@ -1,5 +1,6 @@
+const fs = require('fs')
 const config = require('config')
-const logger = require('@pubsweet/logger')
+const forEach = require('lodash/forEach')
 
 const { accessKeyId, secretAccessKey, bucket } = config.get(
   'pubsweet-server',
@@ -12,6 +13,14 @@ const s3 = new AWS.S3({
   secretAccessKey,
 })
 
+const healthCheck = async () =>
+  new Promise((resolve, reject) => {
+    s3.getBucketLogging({ Bucket: bucket }, (err, data) => {
+      if (err) reject(err)
+      resolve(data)
+    })
+  })
+
 // Returns a an event source for making use of the progress of upload
 const uploadFile = (fileStream, location, filename, mimeType, metadataTag) => {
   // setting up s3 upload parameters
@@ -20,12 +29,10 @@ const uploadFile = (fileStream, location, filename, mimeType, metadataTag) => {
     Key: `${location}/${filename}`, // file name you want to save as
     Body: fileStream,
     ContentType: mimeType,
-    Metadata: {
-      'x-amz-meta-type': metadataTag,
-    },
   }
   return s3.upload(params)
 }
+
 const getFileInfo = async uri => {
   const params = {
     Bucket: bucket,
@@ -54,16 +61,60 @@ const listFiles = async () => {
     })
   })
 }
-// TODO:
-//deleteFile,
-//updateFile,
-//locallyDownloadFile
+
+// Accepts an array of uris
+const deleteFiles = async uris => {
+  const params = {
+    Bucket: bucket,
+    Delete: {
+      Objects: [],
+      Quiet: false,
+    },
+  }
+
+  forEach(uris, uri => {
+    params.Delete.Objects.push({ Key: uri })
+  })
+
+  return new Promise((resolve, reject) => {
+    s3.deleteObjects(params, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(data)
+    })
+  })
+}
+
+const locallyDownloadFile = async (uri, where) => {
+  const fileStream = fs.createWriteStream(where)
+  const s3Stream = s3.getObject({ Bucket: bucket, Key: uri }).createReadStream()
+
+  return new Promise((resolve, reject) => {
+    // Listen for errors returned by the service
+    s3Stream.on('error', err => {
+      // NoSuchKey: The specified key does not exist
+      reject(err)
+    })
+
+    s3Stream
+      .pipe(fileStream)
+      .on('error', err => {
+        // capture any errors that occur when writing data to the file
+        reject(err)
+        console.error('File Stream:', err)
+      })
+      .on('close', () => {
+        resolve()
+      })
+  })
+}
 
 module.exports = {
+  healthCheck,
   uploadFile,
-  // deleteFile,
+  deleteFiles,
   getFileInfo,
   listFiles,
-  // updateFile,
-  // locallyDownloadFile,
+  locallyDownloadFile,
 }
